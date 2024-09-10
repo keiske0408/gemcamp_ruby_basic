286d74b85624417b304febbb5d23f7628fdd82c5
keiske0408 <kieske0408@gmail.com>
2024-09-10 18:47:15 +0800

    q20: adding orange hash to the array

---
+++ array_and_hashes/Act19.rb
@@ -0,0 +1,9 @@
+# Create a hash to represent a library book with the following information: title is "koda", author is "mark", and pages is 100. Print the book's title.
+
+book = {
+  title: "koda",
+  author: "mark",
+  pages: 100
+}
+
+puts book[:title]
---
+++ array_and_hashes/Act20.rb
@@ -0,0 +1,4 @@
+fruits = { apple: 2, banana: 1, cherry: 3 }
+
+fruits[:orange] = 4
+puts fruits
