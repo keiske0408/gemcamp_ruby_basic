fdabf52a6f508ee97e148ebf555ed9b94fa8e40c
keiske0408 <kieske0408@gmail.com>
2024-09-11 11:25:04 +0800

    q28: printing a hash about a favorite movie

---
+++ array_and_hashes/Act28.rb
@@ -0,0 +1,4 @@
+hash_movie =  {title: "private benjamin", director: "vice ganda", year: "2017", rating: 4.5}
+print hash_movie
+
+# Create a hash to store information about your favorite movie: title, director, year, and rating. Print the entire hash.
