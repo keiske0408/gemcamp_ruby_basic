eb5ce6f02eb81ecbacb9bc19ed106a99b5bed7ab
keiske0408 <kieske0408@gmail.com>
2024-09-11 11:10:23 +0800

    q24: adding :job as new key

--- array_and_hashes/Act23.rb
+++ array_and_hashes/Act23.rb
@@ -1,2 +1,2 @@
-fav_books = {"rattatouie": "john", "adventure": "joshua", "lorax":"david" }
+fav_books = {rattatouie: "john", adventure: "joshua", lorax:"david" }
 puts fav_books[:adventure]
---
+++ array_and_hashes/Act24.rb
@@ -0,0 +1,4 @@
+info = { name: "John", age: 25, city: "New York" }
+info[:job]= "developer"
+
+puts info
