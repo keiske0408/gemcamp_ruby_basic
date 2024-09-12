6769bed87d7637b1a84e3636ceee8b214b1581d0
keiske0408 <kieske0408@gmail.com>
2024-09-12 14:29:00 +0800

    q35: replace negative to 0

--- conditional_statements/act_35.rb
+++ conditional_statements/act_35.rb
@@ -1,6 +1,5 @@
 numbers = [10, -5, 3, -2, 0, -7, 8]
 
-# Loop through each number in the array and replace negative numbers with 0
 numbers.map! do |number|
   if number < 0
     0
@@ -9,5 +8,4 @@ numbers.map! do |number|
   end
 end
 
-# Print the updated array
 puts "The updated array is #{numbers}"
